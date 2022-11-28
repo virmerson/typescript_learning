71933c377d1c26638219da10334b5b1be881e054
virmerson <virmerson@gmail.com>
2022-11-28 16:16:50 -0400

    hello world

---
+++ dist/add.js
@@ -0,0 +1,6 @@
+"use strict";
+function addNumber(a, b) {
+    return a + b;
+}
+var sum = addNumber(10, 20);
+console.log(sum);
---
+++ src/add.ts
@@ -0,0 +1,8 @@
+function addNumber (a:  number, b:number){
+    return a+b;
+}
+
+
+var sum: number = addNumber(10,20);
+
+console.log(sum)
